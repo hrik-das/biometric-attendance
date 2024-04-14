5a5b98f13424a103efb70277bf1683c7cccd74b0
playbahn <plabanroy69@gmail.com>
2024-04-14 22:42:39 +0530

    hopefully final db schema

--- server/attendance.sql
+++ server/attendance.sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 14, 2024 at 06:20 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `attendance`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin_cred`
---
 
 CREATE TABLE `admin_cred` (
   `id` int(11) NOT NULL,
@@ -33,18 +13,8 @@ CREATE TABLE `admin_cred` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `admin_cred`
---
-
 INSERT INTO `admin_cred` (`id`, `username`, `password`) VALUES
 (1, 'bas', '12345');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `attendance_log`
---
 
 CREATE TABLE `attendance_log` (
   `s_no` mediumint(8) UNSIGNED NOT NULL,
@@ -52,18 +22,8 @@ CREATE TABLE `attendance_log` (
   `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='RECORDS ONLY PRESENT STUDENTS';
 
---
--- Dumping data for table `attendance_log`
---
-
 INSERT INTO `attendance_log` (`s_no`, `roll_no`, `date`) VALUES
 (1, 5150, '2024-04-14');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users_all`
---
 
 CREATE TABLE `users_all` (
   `fingerprint_id` smallint(4) UNSIGNED NOT NULL,
@@ -74,48 +34,24 @@ CREATE TABLE `users_all` (
   `delist_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Information about all users';
 
---
--- Dumping data for table `users_all`
---
-
 INSERT INTO `users_all` (`fingerprint_id`, `roll_no`, `full_name`, `semester`, `enroll_date`, `delist_date`) VALUES
 (1, 5150, 'Plaban', 6, '2024-04-14', NULL);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin_cred`
---
 ALTER TABLE `admin_cred`
   ADD PRIMARY KEY (`id`);
 
--- Indexes for table `attendance_log`
---
 ALTER TABLE `attendance_log`
   ADD PRIMARY KEY (`s_no`);
 
---
--- Indexes for table `users_all`
---
 ALTER TABLE `users_all`
   ADD PRIMARY KEY (`fingerprint_id`);
 
---
--- AUTO_INCREMENT for table `admin_cred`
---
 ALTER TABLE `admin_cred`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- AUTO_INCREMENT for table `attendance_log`
---
 ALTER TABLE `attendance_log`
   MODIFY `s_no` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `users_all`
---
 ALTER TABLE `users_all`
   MODIFY `fingerprint_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
