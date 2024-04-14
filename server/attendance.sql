32bd5ed3ba3ad71050cb3826b87384656b2ad0a7
playbahn <plabanroy69@gmail.com>
2024-04-14 21:51:43 +0530

    db-new-schema

--- server/attendance.sql
+++ server/attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2024 at 12:18 PM
+-- Generation Time: Apr 14, 2024 at 06:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,28 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendance_log`
+-- Table structure for table `admin_cred`
 --
 
-CREATE TABLE `attendance_log` (
-  `roll_no` smallint(4) UNSIGNED NOT NULL,
-  `f_name` varchar(20) NOT NULL,
-  `m_name` varchar(20) DEFAULT NULL,
-  `l_name` varchar(20) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='RECORDS ONLY PRESENT STUDENTS';
+CREATE TABLE `admin_cred` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_cred`
+--
+
+INSERT INTO `admin_cred` (`id`, `username`, `password`) VALUES
+(1, 'bas', '12345');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `semester_map`
+-- Table structure for table `attendance_log`
 --
 
-CREATE TABLE `semester_map` (
-  `s_no` smallint(4) UNSIGNED NOT NULL,
+CREATE TABLE `attendance_log` (
+  `s_no` mediumint(8) UNSIGNED NOT NULL,
   `roll_no` smallint(4) UNSIGNED NOT NULL,
-  `sem` tinyint(1) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Maps roll nos to semesters';
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='RECORDS ONLY PRESENT STUDENTS';
+
+--
+-- Dumping data for table `attendance_log`
+--
+
+INSERT INTO `attendance_log` (`s_no`, `roll_no`, `date`) VALUES
+(1, 5150, '2024-04-14');
 
 -- --------------------------------------------------------
 
@@ -56,21 +68,27 @@ CREATE TABLE `semester_map` (
 CREATE TABLE `users_all` (
   `fingerprint_id` smallint(4) UNSIGNED NOT NULL,
   `roll_no` smallint(4) UNSIGNED NOT NULL,
-  `f_name` varchar(20) NOT NULL,
-  `m_name` varchar(20) DEFAULT NULL,
-  `l_name` varchar(20) NOT NULL,
+  `full_name` varchar(30) NOT NULL,
+  `semester` tinyint(1) UNSIGNED NOT NULL,
   `enroll_date` date NOT NULL DEFAULT current_timestamp(),
   `delist_date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Information about all users';
+
+--
+-- Dumping data for table `users_all`
+--
+
+INSERT INTO `users_all` (`fingerprint_id`, `roll_no`, `full_name`, `semester`, `enroll_date`, `delist_date`) VALUES
+(1, 5150, 'Plaban', 6, '2024-04-14', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `semester_map`
+-- Indexes for table `attendance_log`
 --
-ALTER TABLE `semester_map`
+ALTER TABLE `attendance_log`
   ADD PRIMARY KEY (`s_no`);
 
 --
@@ -84,16 +102,16 @@ ALTER TABLE `users_all`
 --
 
 --
--- AUTO_INCREMENT for table `semester_map`
+-- AUTO_INCREMENT for table `attendance_log`
 --
-ALTER TABLE `semester_map`
-  MODIFY `s_no` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `attendance_log`
+  MODIFY `s_no` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users_all`
 --
 ALTER TABLE `users_all`
-  MODIFY `fingerprint_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `fingerprint_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
