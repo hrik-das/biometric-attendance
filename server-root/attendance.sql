effb93d88df90dfcad7538052fb90d38ecc985ed
playbahn <plabanroy69@gmail.com>
2024-04-14 15:49:32 +0530

    db-install

---
+++ server-root/attendance.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2024 at 12:18 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `attendance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_log`
+--
+
+CREATE TABLE `attendance_log` (
+  `roll_no` smallint(4) UNSIGNED NOT NULL,
+  `f_name` varchar(20) NOT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='RECORDS ONLY PRESENT STUDENTS';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semester_map`
+--
+
+CREATE TABLE `semester_map` (
+  `s_no` smallint(4) UNSIGNED NOT NULL,
+  `roll_no` smallint(4) UNSIGNED NOT NULL,
+  `sem` tinyint(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Maps roll nos to semesters';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_all`
+--
+
+CREATE TABLE `users_all` (
+  `fingerprint_id` smallint(4) UNSIGNED NOT NULL,
+  `roll_no` smallint(4) UNSIGNED NOT NULL,
+  `f_name` varchar(20) NOT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) NOT NULL,
+  `enroll_date` date NOT NULL DEFAULT current_timestamp(),
+  `delist_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `semester_map`
+--
+ALTER TABLE `semester_map`
+  ADD PRIMARY KEY (`s_no`);
+
+--
+-- Indexes for table `users_all`
+--
+ALTER TABLE `users_all`
+  ADD PRIMARY KEY (`fingerprint_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `semester_map`
+--
+ALTER TABLE `semester_map`
+  MODIFY `s_no` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_all`
+--
+ALTER TABLE `users_all`
+  MODIFY `fingerprint_id` smallint(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
