0f2dfd2caf0c9cee570cf0dd7996506df4a44384
hrik-das <emptynull01@gmail.com>
2024-04-14 15:50:40 +0530

    Initial Commit

---
+++ server/include/include.php
@@ -0,0 +1,5 @@
+<link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
+<link rel="stylesheet" href="./css/common.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
---
+++ server/index.php
@@ -0,0 +1,52 @@
+<?php
+    require("./include/connect.php");
+    require("./include/essential.php");
+    session_start();
+    if((isset($_SESSION["adminLogin"]) && $_SESSION["adminLogin"] == true)){
+        header("Location: dashboard.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <?php require("./include/include.php"); ?>
+    <title>Admin Login Panel</title>
+</head>
+<body class="bg-light">
+    <div class="login-form text-center rounded bg-white shadow overflow-hidden">
+        <form action="index.php" method="post">
+            <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
+            <div class="p-4">
+                <div class="mb-3">
+                    <input type="text" name="name" class="form-control shadow-none text-center" placeholder="Username" required>
+                </div>
+                <div class="mb-4">
+                    <input type="password" name="pass" class="form-control shadow-none text-center" placeholder="Password" required>
+                </div>
+                <button type="submit" name="login" class="btn text-white btn-dark shadow-none">LOGIN</button>
+            </div>
+        </form>
+    </div>
+
+    <?php
+        if(isset($_POST["login"])){
+            $filterData = filteration($_POST);
+            $query = "SELECT * FROM `admin_cred` WHERE `username`=? AND `password`=?";
+            $values = [$filterData["username"], $filterData["password"]];
+            $datatypes = "ss";
+            $result = selectData($query, $values, $datatypes);
+            if($result->num_rows == 1){
+                $data = mysqli_fetch_assoc($result);
+                $_SESSION["adminId"] = $data["id"];
+                $_SESSION["adminLogin"] = true;
+                redirect("dashboard.php");
+            }else{
+                alert("error", "Login failed - Invalid Credentials!");
+            }
+        }
+    ?>
+</body>
+</html>
