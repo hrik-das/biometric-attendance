b0b1561cdaa46d647e7447e6e091568b8ec7133c
hrik-das <emptynull01@gmail.com>
2024-04-14 22:11:28 +0530

    New Student Details Feature Added

--- server/attendance.sql
+++ server/attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2024 at 04:57 PM
+-- Generation Time: Apr 14, 2024 at 06:40 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
