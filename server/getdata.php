d025d9a7efb24afbaaaf8ff5a9edef8e3e228694
playbahn <plabanroy69@gmail.com>
2024-04-15 16:28:36 +0530

    Refine getdata/login util, rm connect/redundant fn

--- server/getdata.php
+++ server/getdata.php
@@ -3,42 +3,37 @@ require 'include/connect.php';
 
 // ATTENDANCE
 if (isset($_POST['fingerprint_id'])) {
-    // check if student is enlisted or delisted
+    // get roll number of student
     $query = "SELECT `roll_no`
-                FROM `users_all`
-                WHERE `fingerprint_id` = ? AND `delist_date` IS NULL;";
-
+                FROM `users_all`            -- delisted student's print WON'T be matched
+                WHERE `fingerprint_id` = ?; -- AND `delist_date` IS NULL;";
     $users_all_arr = execCRUD($query, "i", $_POST['fingerprint_id']);
-
-    if ( // student is delisted
-        !($users_all_row = $users_all_arr->fetch_assoc())
-    ) exit();
-    
-    // FOR THE MCU
-    // student is enlisted, enough to greet the nigga "welcome"
-    echo "login".$users_all_row['roll_no'];
 
     // check if student has already logged into the daily records
     $query = "SELECT *
                 FROM `attendance_log`
                 WHERE `roll_no` = ? AND `date` = CURRENT_DATE();";
-
     $attendance_log_arr = execCRUD($query, "i", $users_all_row['roll_no']);
     
     if ( // student student has already logged in
         !($attendance_log_row = $attendance_log_arr->fetch_assoc())
-    ) exit();
-
+    ) {
+        echo "login".$users_all_row['roll_no'];
+        exit($users_all_row['roll_no']." already logged in.");
+    }
+    
     // otherwise, log student into the daily records
     $query = "INSERT INTO attendance_log (roll_no) VALUES (?)";
-
     $inserted_rows = execCRUD($query, "i", $attendance_log_row['roll_no']);
 
     // no rows inserted, something is wrong.
     if ($inserted_rows != 1) {
-        die("Failure executing query: INSERT INTO attendance_log (roll_no) VALUES ("
-        .$attendance_log_row['roll_no'].")");
+        die("Failure executing query: "
+        ."INSERT INTO attendance_log (roll_no) VALUES (".$attendance_log_row['roll_no'].")");
     }
+
+    echo "login".$users_all_row['roll_no'];
+    exit($users_all_row['roll_no']." logged in just now.");
 }
 
 // ADD USER
--- server/include/connect.php
+++ server/include/connect.php
@@ -27,8 +27,9 @@
     }
 
     /**
-     * In-house functionality for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`) operations
-     * @param string $query An SQL query
+     * In-house utility for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`) operations. Refrain
+     * from passing nested subqueries to this function for easier error checking.
+     * @param string $query SQL `INSERT`, `SELECT`, `UPDATE` or `DELETE` query
      * @param string $types
      * @param mixed $vars
      * @return \mysqli_result|false|int|string
@@ -39,14 +40,14 @@
         $connect = $GLOBALS["connect"];
         $stmt = mysqli_prepare($connect, $query);
         if (!$stmt)
-            die("Error: `mysqli_prepare` at Line #31: query: $query");
+            die("Error: `mysqli_prepare` at include/connect.php: Line #31: query: $query");
         
         if (!$stmt->bind_param($types, ...$vars))
-            die("Error: `stmt->bind_param` at Line #35: query: $query");
+            die("Error: `stmt->bind_param` at include/connect.php: Line #35: query: $query");
 
         if (!$stmt->execute()) {
             $stmt->close();
-            die("Error: `stmt->execute` at Line #38: query: $query");
+            die("Error: `stmt->execute` at include/connect.php: Line #38: query: $query");
         }
         
         if ($query[0] == 'S' || $query[0] == 's') {
@@ -57,73 +58,5 @@
         
         $stmt->close();
         return $result;
-    }
-
-    function insertData($sql, $values, $datatypes){
-        $connect = $GLOBALS["connect"];
-        if($stmt = mysqli_prepare($connect, $sql)){
-            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_affected_rows($stmt);
-                mysqli_stmt_close($stmt);
-                return $result;
-            }else{
-                mysqli_stmt_close($stmt);
-                die("Query Cannot be Executed - Insert");
-            }
-        }else{
-            die("Query Cannot be Prepared - Insert");
-        }
-    }
-
-    function selectData($sql, $values, $datatypes){
-        $connect = $GLOBALS["connect"];
-        if($stmt = mysqli_prepare($connect, $sql)){
-            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_get_result($stmt);
-                mysqli_stmt_close($stmt);
-                return $result;
-            }else{
-                mysqli_stmt_close($stmt);
-                die("Query Cannot be Executed - Select");
-            }
-        }else{
-            die("Query Cannot be Prepared - Select");
-        }
-    }
-
-    function updateData($sql, $values, $datatypes){
-        $connect = $GLOBALS["connect"];
-        if($stmt = mysqli_prepare($connect, $sql)){
-            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_affected_rows($stmt);
-                mysqli_stmt_close($stmt);
-                return $result;
-            }else{
-                mysqli_stmt_close($stmt);
-                die("Query Cannot be Executed - Update");
-            }
-        }else{
-            die("Query Cannot be Prepared - Update");
-        }
-    }
-
-    function deleteData($sql, $values, $datatypes){
-        $connect = $GLOBALS["connect"];
-        if($stmt = mysqli_prepare($connect, $sql)){
-            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_affected_rows($stmt);
-                mysqli_stmt_close($stmt);
-                return $result;
-            }else{
-                mysqli_stmt_close($stmt);
-                die("Query Cannot be Executed - Delete");
-            }
-        }else{
-            die("Query Cannot be Prepared - Delete");
-        }
     }
 ?>
