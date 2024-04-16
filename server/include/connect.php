cc5617334c89b4c9a2ab6009f0b808844bcc9c46
hrik-das <emptynull01@gmail.com>
2024-04-16 19:20:39 +0530

    Nothing Much

--- server/include/connect.php
+++ server/include/connect.php
@@ -35,14 +35,6 @@
         * @return \mysqli_result|false|int|string
         * Returns a `\mysqli_result` object or `false` for a sucessful or unsucessful `SELECT`
         * `$query` respectively, `int` or `string` for no. of affected rows for the other queries
-     * In-house utility for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`) operations. Refrain
-     * from passing nested subqueries to this function for easier error checking.
-     * @param string $query SQL `INSERT`, `SELECT`, `UPDATE` or `DELETE` query
-     * @param string $types
-     * @param array ...$vars
-     * @return \mysqli_result|false|int|string
-     * Returns a `\mysqli_result` object or `false` for a sucessful or unsucessful `SELECT`
-     * `$query` respectively, `int` or `string` for no. of affected rows for the other queries
      */
 
     function execCRUD($query, $types, ...$vars){
@@ -51,10 +43,10 @@
         if (!$stmt)
             die("Error: `mysqli_prepare` at include/connect.php: Line #41: query: $query");
         
-        if (!$stmt->bind_param($types, ...$vars))
+        else if (!$stmt->bind_param($types, ...$vars))
             die("Error: `stmt->bind_param` at include/connect.php: Line #45: query: $query");
 
-        if (!$stmt->execute()) {
+        else if (!$stmt->execute()) {
             $stmt->close();
             die("Error: `stmt->execute()` at include/connect.php: Line #48: query: $query");
         }
