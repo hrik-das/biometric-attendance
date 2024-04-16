40e6bd471ad20d32cb2ba5b74c874da71f96438e
playbahn <plabanroy69@gmail.com>
2024-04-16 16:29:51 +0530

    Change execCRUD call in index/php Insiginificant changes to getdata & connect

--- server/include/connect.php
+++ server/include/connect.php
@@ -27,40 +27,36 @@
     }
 
     /**
-        * In-house utility for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`) operations. Refrain
-        * from passing nested subqueries to this function for easier error checking.
-        * @param string $query SQL `INSERT`, `SELECT`, `UPDATE` or `DELETE` query
-        * @param string $types
-        * @param mixed $vars
-        * @return \mysqli_result|false|int|string
-        * Returns a `\mysqli_result` object or `false` for a sucessful or unsucessful `SELECT`
-        * `$query` respectively, `int` or `string` for no. of affected rows for the other queries
-     * In-house utility for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`) operations. Refrain
-     * from passing nested subqueries to this function for easier error checking.
-     * @param string $query SQL `INSERT`, `SELECT`, `UPDATE` or `DELETE` query
-     * @param string $types
-     * @param array ...$vars
-     * @return \mysqli_result|false|int|string
-     * Returns a `\mysqli_result` object or `false` for a sucessful or unsucessful `SELECT`
-     * `$query` respectively, `int` or `string` for no. of affected rows for the other queries
-     */
-
-    function execCRUD($query, $types, ...$vars){
+    * In-house utility for simple CRUD (`INSERT`, `SELECT`, `UPDATE`, `DELETE`)
+    * operations. Refrain from passing nested subqueries to this function for
+    * easier error checking.
+    * @param string $query SQL `INSERT`, `SELECT`, `UPDATE` or `DELETE` query
+    * @param string $types type string
+    * @param array ...$vars UNPACKED array, or simply comma-separated-values
+    * @return \mysqli_result|false|int|string
+    * Returns a `\mysqli_result` object or `false` for a sucessful or unsucessful
+    * `SELECT` `$query` respectively, `int` or `string` for no. of affected rows
+    * for the other queries
+    */
+    function execCRUD($query, $types, ...$vars) {
         $connect = $GLOBALS["connect"];
         $stmt = mysqli_prepare($connect, $query);
         if (!$stmt)
-            die("Error: `mysqli_prepare` at include/connect.php: Line #41: query: $query");
+            die("Error: `mysqli_prepare()` at include/connect.php: Line #43:" .
+            "\nquery: $query");
         
         if (!$stmt->bind_param($types, ...$vars))
-            die("Error: `stmt->bind_param` at include/connect.php: Line #45: query: $query");
+            die("Error: `stmt->bind_param()` at include/connect.php: Line #48:" .
+            "\nquery: $query\ntypes: $types\nvars: $vars");
 
         if (!$stmt->execute()) {
             $stmt->close();
-            die("Error: `stmt->execute()` at include/connect.php: Line #48: query: $query");
+            die("Error: `stmt->execute()` at include/connect.php: Line #52:" .
+            "\nquery: $query");
         }
-        if($query[0] == "S" || $query[0] == "s"){
+        if ($query[0] == "S" || $query[0] == "s"){
             $result = $stmt->get_result();
-        }else{
+        } else {
             $result = $stmt->affected_rows;
         }
 
